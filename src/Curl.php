05ac1b4fde548545fdf334172102b330cf3d048e
Alexis CHEMEL <chemel@users.noreply.github.com>
2016-12-07 14:15:28 +0100

    Update chrome UA

--- src/Curl.php
+++ src/Curl.php
@@ -225,7 +225,7 @@ class Curl implements CurlInterface {
      */
     public function getChromeUserAgent() {
 
-        return 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36';
+        return 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36';
     }
 
     /**
